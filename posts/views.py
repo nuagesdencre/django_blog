2e8e78744cce68d6b1c829d1a5983dafe120992c
Veronique SQ <fizzbones@gmail.com>
2018-11-18 20:26:37 +0000

    attempted travis fix

--- posts/views.py
+++ posts/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
-  
+
+
 def get_posts(request):
     """
-    Create a view that will return a list of posts
-    and render it using the blogposts.html template.
+    Create a view that will return a
+    list of Posts that were published prior to'now'
+    and render them to the 'blogposts.html' template
     """
-    posts = Post.objects.filter(published_date_lt_lte=timezone.now().order_by('-published_date'))
-    return render(request, "blogposts.html", {'posts':posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+                                ).order_by('-published_date')
+    return render(request, "blogposts.html", {'posts': posts})
     
 def post_details(request, pk):
     """
