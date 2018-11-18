294b643726422d7979d154c06c0b97c797778bd4
Veronique SQ <fizzbones@gmail.com>
2018-11-18 17:46:29 +0000

    created views and urls

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from .views import get_posts, post_details, create_or_edit_post
+
+urlpatterns = [
+    url(r'^$',get_posts, name='get_posts'),
+    url(r'^(?P<pk>\d+)/$', post_details, name='post_details'),
+    url(r'^new/$', create_or_edit_post, name='new_post'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_post, name='edit_post')
+        
+        ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import BlogPostForm
+  
+def get_posts(request):
+    """
+    Create a view that will return a list of posts
+    and render it using the blogposts.html template.
+    """
+    posts = Post.objects.filter(published_date_lt_lte=timezone.now().order_by('-published_date'))
+    return render(request, "blogposts.html", {'posts':posts})
+    
+def post_details(request, pk):
+    """
+    Create a view of a single post according to an id (pk)
+    and render it using the postdetails.html template.
+    """
+    post = get_object_or_404(Post, pk=pk)
+    post.views+=1
+    post.save()
+    return render(request, "postdetails.html", {'post':post})
+    
+def create_or_edit_post(request, pk=None):
+    """
+    A view that allows the creation or edition of a post if id(pk) is not None
+    """
+    post = get_object_or_404(Post, pk=pk) if pk else None
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post= form.save()
+            return redirect(post_details, post.pk)
+        else:
+            form = BlogPostForm(instance=post)
+        return render(request, "blogpostform.html", {'form':form})
