f4f185c98788d63e7352141323a814b2b53845f3
Veronique SQ <fizzbones@gmail.com>
2018-11-18 22:04:31 +0000

    getting travis to pass tests successfully

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 
 os.environ.setdefault('SECRET_KEY',"'70zksp&!o_jwellna%fy+mzbpp9j$20^c5tq-qrghib2%q0*&d'")
-os.environ.setdefault("DATABASE_URL", "'postgres://mfjvdkjtrcvgsg:2140d506ec1b66d65cb2564112a6c07e785e7e74fba40db0ebc483f1f5f7f3fa@ec2-54-75-231-3.eu-west-1.compute.amazonaws.com:5432/dar1vbcp4nlc6d'")
+os.environ.setdefault('DATABASE_URL', "'postgres://mfjvdkjtrcvgsg:2140d506ec1b66d65cb2564112a6c07e785e7e74fba40db0ebc483f1f5f7f3fa@ec2-54-75-231-3.eu-west-1.compute.amazonaws.com:5432/dar1vbcp4nlc6d'")
